09af04c8b44ea9ad68c51278816a273fe2cba112
JhonatanSierra <jhonatanjavier034@gmail.com>
2025-05-22 12:05:25 -0500

    Codigo_Maestro.py

--- Codigo_Maestro.py
+++ Codigo_Maestro.py
@@ -1,37 +1,59 @@
-from machine import Pin, UART
 import utime
+import time
+import network
+import socket
 
-# Configurar UART1 (puedes usar UART0 si quieres otros pines)
-uart = UART(1, baudrate=9600, tx=Pin(4), rx=Pin(5))
+timeout = 10  # segundos
+start = time.time()
+# Configurar Wi-Fi con IP fija
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.ifconfig((
+    '192.168.1.100',  # IP del maestro
+    '255.255.255.0',
+    '192.168.1.1',
+    '192.168.1.1'
+))
+wlan.connect('APMAESTRO', '12345678')
 
-# Pin para controlar dirección (DE/RE unidos)
-dir_ctrl = Pin(6, Pin.OUT)
+#while not wlan.isconnected():
+#    if time.time() - start > timeout:
+#        print("❌ No se pudo conectar al Wi-Fi.")
+#        break
+#    time.sleep(0.5)
 
-# Funciones auxiliares
-def enviar_mensaje(mensaje):
-    dir_ctrl.value(1)  # Activar modo transmisión
-    utime.sleep_us(20) # Pequeña espera para estabilidad
-    uart.write(mensaje + '\n')  # Enviar mensaje con nueva línea
-    utime.sleep_us(20)
-    dir_ctrl.value(0)  # Volver a modo recepción
+while not wlan.isconnected():
+    print("No conectado")
+    utime.sleep(0.5)
+    pass
 
-# Inicialmente, modo recepción
-dir_ctrl.value(0)
+print('Maestro conectado. IP:', wlan.ifconfig())
 
-print("Nodo Pico W listo. Escuchando mensajes...")
+# Crear socket UDP
+udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+udp.bind(('0.0.0.0', 5005))  # Escuchar en el puerto 5005
 
-contador = 0
+valores_nodos = [None] * 9  # ID de nodos: 1 a 8
+
+print("Esperando datos de los nodos...")
 
 while True:
-    # Escuchar mensajes entrantes
-    if uart.any():
-        data = uart.readline()
-        if data:
-            print("Mensaje recibido:", data.decode().strip())
+    data, addr = udp.recvfrom(1024)
+    try:
+        mensaje = data.decode().strip()
+        #print(f"Recibido: {mensaje} desde {addr}")
+        
+        id_str, valor_str = mensaje.split(":")
+        nodo_id = int(id_str)
+        
+        if 1 <= nodo_id <= 8:
+            valores_nodos[nodo_id] = valor_str
+            print(f"R{nodo_id} = {valor_str}")
+            #print(f"Nodo {nodo_id} = {valor_str})
+        else:
+            print("ID fuera de rango")
+    
+    except Exception as e:
+        print("Error:", e)
 
-    # Cada 5 segundos, enviar un mensaje
-    if contador % 50 == 0:
-        enviar_mensaje("Hola desde nodo RS-485")
-
-    utime.sleep(0.1)
-    contador += 1
+    #print("Valores actuales:", valores_nodos[1:])
